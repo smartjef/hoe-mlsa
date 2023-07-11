179af0bd1698203417ab0a2b170256e5dcc3040c
OMOLLO PINCHEZ <masimevanistelrooy@gmail.com>
2023-07-11 11:01:47 +0300

    new

--- Rovy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.Home,name='Home'),
-     path('cv/',views.cv,name='cv'),
-      path('hire-me/',views.hireme,name='hireme'),
-       path('projects/',views.projects,name='projects'),
-]
